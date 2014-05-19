98b040791cb22a5040e7a991ca007ddd068b7630
Nitish Hooda <thenitish23121991@yahoo.in>
2014-05-19 13:08:35 +0530

    Changed html to PHP

---
+++ home.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+
+<body>
+
+
+<div class="container">
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<h2></h2>
+<img src="" class="" alt="" />
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=">
+<h2></h2>
+<img src="" class="" alt="" />
+</a>
+</div>
+
+</div>
+
+
+</body>
+</html>
