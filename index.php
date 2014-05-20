030d93943e9bc014871854f3d7a49155ecc1c482
Nitish Hooda <nitsh7084@gmail.com>
2014-05-20 15:23:33 +0530

    Added all movies

--- index.php
+++ index.php
@@ -11,115 +11,250 @@
 
 <div class="container">
 
+<div class="all_videos">
+<img src="CinemaBox logo.jpg" alt="CinemaBox" class="CinemaBox" />
+</div>
 <div class="inner_container">
 
-<div class="all_videos">
-All Videos
-</div>
-
-<div class="video_container">
-<a href="video.php?video=Ragini MMS 2">
-<h2>Ragini MMS 2</h2>
-<img src="still.jpg" class="still" alt="still" />
-</a>
-</div>
-
-<div class="video_container">
-<a href="video.php?video=Revolver Rani">
-<h2>Revolver Rani</h2>
-<img src="still.jpg" class="still" alt="still" />
-</a>
-</div>
-
-<div class="video_container">
-<a href="video.php?video=Main Tera Hero">
-<h2>Main Tera Hero</h2>
-<img src="still.jpg" class="still" alt="still" />
-</a>
-</div>
-
-<div class="video_container">
-<a href="video.php?video=American Hustle">
-<h2>American Hustle</h2>
-<img src="still.jpg" class="still" alt="still" />
-</a>
-</div>
 
 
 <div class="video_container">
-<a href="video.php?video=Gulaab Gang">
-<h2>Gulaab Gang</h2>
-<img src="still.jpg" class="still" alt="still" />
-</a>
-</div>
-
-<div class="video_container">
-<a href="video.php?video=Django Unchained">
-<h2>Django Unchained</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=2 states">
+<h2>2 states</h2>
+<img src="2 states.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
 <a href="video.php?video=Queen">
 <h2>Queen</h2>
-<img src="still.jpg" class="still" alt="still" />
+<img src="Queen.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
-<a href="video.php?video=Thor 2">
-<h2>Thor 2</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=Highway">
+<h2>Highway</h2>
+<img src="Highway.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
-<a href="video.php?video=Bewakoofiyaan">
-<h2>Bewakoofiyaan</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=Hasee To Phasee">
+<h2>Hasee To Phasee</h2>
+<img src="Hasee To Phasee.jpg" class="still" alt="still" />
+</a>
+</div>
+
+
+<div class="video_container">
+<a href="video.php?video=Ragini MMS 2">
+<h2>Ragini MMS 2</h2>
+<img src="Ragini MMS 2.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
-<a href="video.php?video=Robocop">
-<h2>Robocop</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=Main Tera Hero">
+<h2>Main Tera Hero</h2>
+<img src="Main Tera Hero.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Jai Ho">
+<h2>Jai Ho</h2>
+<img src="Jai Ho.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Revolver Rani">
+<h2>Revolver Rani</h2>
+<img src="Revolver Rani.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Gunday">
+<h2>Gunday</h2>
+<img src="Gunday.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Gulaab Gang">
+<h2>Gulaab Gang</h2>
+<img src="Gulaab Gang.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Dedh Ishqiya">
+<h2>Dedh Ishqiya</h2>
+<img src="Dedh Ishqiya.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
 <a href="video.php?video=Bhootnath Returns">
 <h2>Bhootnath Returns</h2>
-<img src="still.jpg" class="still" alt="still" />
+<img src="Bhootnath Returns.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
-<a href="video.php?video=Gori Tere Pyaar Main">
-<h2>Gori Tere Pyaar Main</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=Bhaag Milkha Bhaag">
+<h2>Bhaag Milkha Bhaag</h2>
+<img src="Bhaag Milkha Bhaag.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
-<a href="video.php?video=Hasee to Phasee">
-<h2>Hasee to Phasee</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=Ye Jawaani Hai Deewani">
+<h2>Ye Jawaani Hai Deewani</h2>
+<img src="Ye Jawaani Hai Deewani.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Aashiqui 2">
+<h2>Aashiqui 2</h2>
+<img src="Aashiqui 2.jpg" class="still" alt="still" />
+</a>
+</div>
+
+
+<div class="video_container">
+<a href="video.php?video=Bullett Raja">
+<h2>Bullett Raja</h2>
+<img src="Bullett Raja.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Chennai Express">
+<h2>Chennai Express</h2>
+<img src="Chennai Express.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Bewakoofiyaan">
+<h2>Bewakoofiyaan</h2>
+<img src="Bewakoofiyaan.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Ram Leela">
+<h2>Ram Leela</h2>
+<img src="Ram Leela.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Lootera">
+<h2>Lootera</h2>
+<img src="Lootera.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Ishaqzaade">
+<h2>Ishaqzaade</h2>
+<img src="Ishaqzaade.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Dhoom 3">
+<h2>Dhoom 3</h2>
+<img src="Dhoom 3.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=D day">
+<h2>D day</h2>
+<img src="D day.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Aurangzeb">
+<h2>Aurangzeb</h2>
+<img src="Aurangzeb.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Agneepath">
+<h2>Agneepath</h2>
+<img src="Agneepath.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
 <a href="video.php?video=The Lunchbox">
 <h2>The Lunchbox</h2>
-<img src="still.jpg" class="still" alt="still" />
+<img src="The Lunchbox.jpg" class="still" alt="still" />
 </a>
 </div>
 
 <div class="video_container">
-<a href="video.php?video=Shuddh Desi Romance">
-<h2>Shuddh Desi Romance</h2>
-<img src="still.jpg" class="still" alt="still" />
+<a href="video.php?video=Raanjhaana">
+<h2>Raanjhaana</h2>
+<img src="Raanjhaana.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Madras Cafe">
+<h2>Madras Cafe</h2>
+<img src="Madras Cafe.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Satyagraha">
+<h2>Satyagraha</h2>
+<img src="Satyagraha.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Django Unchained">
+<h2>Django Unchained</h2>
+<img src="Django Unchained.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=American Hustle">
+<h2>American Hustle</h2>
+<img src="American Hustle.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Skyfall">
+<h2>Skyfall</h2>
+<img src="Skyfall.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Captain Philips">
+<h2>Captain Philips</h2>
+<img src="Captain Philips.jpg" class="still" alt="still" />
+</a>
+</div>
+
+<div class="video_container">
+<a href="video.php?video=Thor 2">
+<h2>Thor 2</h2>
+<img src="Thor 2.jpg" class="still" alt="still" />
 </a>
 </div>
 
